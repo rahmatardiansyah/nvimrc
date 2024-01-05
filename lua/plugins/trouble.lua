e1dcdab4888c48a10fe27da917641a7880cbf9ef
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-01-05 14:57:02 +0700

    update: update config trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,10 +2,15 @@ return {
 	enabled = true,
 	'folke/trouble.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = {},
 	config = function()
+		local trouble = require('trouble')
+
+		trouble.setup({
+			cycle_results = false,
+		})
+
 		vim.keymap.set('n', '<leader>xx', function()
-			require('trouble').toggle()
+			trouble.toggle()
 		end)
 	end,
 }
