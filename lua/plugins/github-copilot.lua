685f390888e9592edbc6467870f359d37f9b3bb5
Rahmat Ardiansyah <artsbymat@gmail.com>
2024-12-26 09:22:34 +0700

    update

---
+++ lua/plugins/github-copilot.lua
@@ -0,0 +1,4 @@
+return {
+  enabled = true,
+  'github/copilot.vim',
+}
--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -6,7 +6,5 @@ return {
     require('leap').opts.safe_labels = {}
 
     vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
-    vim.keymap.del({ 'x', 'o' }, 'x')
-    vim.keymap.del({ 'x', 'o' }, 'X')
   end,
 }
